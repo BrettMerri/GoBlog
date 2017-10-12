c47758e749208b627543fb2b03ac26aa82f28537
Brett Merrifield <the.b.merri@gmail.com>
2017-10-12 11:13:03 -0400

    added bootstrap to articles.go which specifies unique key to collection

--- handlers/articles/articles.go
+++ handlers/articles/articles.go
@@ -9,12 +9,15 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+// Read : read article from DB
 func Read(c *gin.Context) {
 	db := c.MustGet("db").(*mgo.Database)
 
 	article := models.Article{}
 
-	err := db.C(models.CollectionArticle).Find(bson.M{"title": "Ale"}).One(&article)
+	col := bootstrap(db)
+
+	err := col.Find(bson.M{"title": "Ale"}).One(&article)
 
 	if err != nil {
 		fmt.Printf("Can't find article, go error %v\n", err)
@@ -24,10 +27,13 @@ func Read(c *gin.Context) {
 	c.JSON(200, article)
 }
 
+// Add : add article from DB
 func Add(c *gin.Context) {
 	db := c.MustGet("db").(*mgo.Database)
 
-	err := db.C(models.CollectionArticle).Insert(models.Article{Title: "Ale", Body: "+55 53 1234 4321"},
+	col := bootstrap(db)
+
+	err := col.Insert(models.Article{Title: "Ale", Body: "+55 53 1234 4321"},
 		models.Article{Title: "Cla", Body: "+66 33 1234 5678"})
 
 	if err != nil {
@@ -38,4 +44,18 @@ func Add(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"result": true,
 	})
+}
+
+func bootstrap(db *mgo.Database) *mgo.Collection {
+	c := db.C(models.CollectionArticle)
+
+	index := mgo.Index{
+		Key:        []string{"name"},
+		Unique:     true,
+		Background: true,
+	}
+
+	c.EnsureIndex(index)
+
+	return c
 }
